f6ec16b5f8a2802d2267912f25e4786b8f66036e
aungkaungkhant-nssh <akkgit0909@gmail.com>
2024-12-13 19:48:24 +0630

    change title

--- components/todos/TodoLists.tsx
+++ components/todos/TodoLists.tsx
@@ -7,7 +7,7 @@ import CreateTodoForm from "./CreateTodoForm";
 export default function TodoLists({ todos }: TodosProps) {
     return (
         <div className="w-[29%]">
-            <h1 className="font-bold text-2xl text-center text-slate-700 mb-3">Next Js And Laravel Todo Lists</h1>
+            <h1 className="font-bold text-2xl text-center text-slate-700 mb-3">Next Js  Todo Lists</h1>
             <Card>
                 <CreateTodoForm />
                 {
